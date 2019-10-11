848949013ab06562496a567052b91415d6bda14e
nihui <18202504057@163.com>
2019-10-11 15:04:10 +0800

    init commit

---
+++ Shiro-Limit/src/main/java/com/nh/limit/ShiroLimitApplication.java
@@ -0,0 +1,13 @@
+package com.nh.limit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroLimitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroLimitApplication.class, args);
+    }
+
+}
